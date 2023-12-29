56a0ca5293ddce6d9cf5b120441e355c0dd12c25
sithu <sithuminhtet67@gmail.com>
2023-12-29 11:12:09 +0630

    Hello World for first time in git

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+
+})
+
+// const handleOnMouseMove = e => {
+// const {currentTarget: target} = e;
+
+// const rect = target.getBoundingClientReact(),
+// x = e.clientX - rect.left,
+// y = e.clientY - rect.top;
+// target.style.setProperty("--mouse-x", '${x}px');
+// target.style.setProperty("--mouse-y", '${y}px');
+// }
+
+// for(const glass of document.querySelectorAll(".glass")) {
+//     glass.onmousemove = e => handleOnMouseMove(e);
+// }
