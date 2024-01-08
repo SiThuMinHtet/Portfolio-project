73041f9889c0bb50c34a0b2df26f4e0e4e571025
Toe Tet <damon@MacBook-Air.local>
2024-01-08 14:30:47 +0630

    vscode: --align

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
-
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-
-})
+	hamburger.classList.toggle("active");
+	navMenu.classList.toggle("active");
+});
 
 // const handleOnMouseMove = e => {
 // const {currentTarget: target} = e;
